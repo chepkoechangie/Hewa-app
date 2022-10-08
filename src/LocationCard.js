20e958c4f42495c27461fc803ac4575001775ac0
Angela <chepkoechangela514@gmail.com>
2022-10-08 19:01:50 +0300

    return card

--- src/LocationCard.js
+++ src/LocationCard.js
@@ -1,2 +1,24 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+
+function LocationCard() {
+    return (
+      <Card
+        style={{
+          width: "80vw",
+          margin: "2rem auto",
+          height: "auto",
+          padding: "2rem",
+        }}
+      >
+        <Card.Body>
+          <h1>Weather App coming soon!</h1>
+          <Card.Img
+            //   style={{ width: "50vw", height: "60vh", padding: "2rem" }}
+            src={process.env.PUBLIC_URL + "weather.png"}
+            alt="img"
+          />
+        </Card.Body>
+      </Card>
+    );
+  }
