1dc1321f91d0ce92970f5506b806a72a188ac291
Angela <chepkoechangela514@gmail.com>
2022-10-08 18:57:50 +0300

    classnames

--- src/App.js
+++ src/App.js
@@ -2,3 +2,13 @@ import React from "react";
 import NavbarComponent from "./NavbarComponent";
 import LocationCard from "./LocationCard";
 import FullWeek from "./FullWeek";
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent />
+      <LocationCard />
+      <FullWeek />
+    </div>
+  );
+}
